430c08c00b51d51a53c72ec0f0ca66afaf22c9ef
Nayefbah <nayef.rind@gmail.com>
2024-10-13 18:23:03 +0300

    Updated

--- app.js
+++ app.js
@@ -71,11 +71,3 @@ const performCalculation = (num1, num2, operator) => {
     return num1
   }
 }
-
-buttons.forEach((button) => {
-  button.addEventListener('click', (event) => {
-    // This log is for testing purposes to verify we're getting the correct value
-    //console.log(event.target.innerText)
-    // Future logic to capture the button's value would go here...
-  })
-})
